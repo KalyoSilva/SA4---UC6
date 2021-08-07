f89c1e58927db54bda5eecb5afd609656ad6566f
KalyoSilva <85971839+KalyoSilva@users.noreply.github.com>
2021-08-07 16:56:04 -0300

    V 0.2

--- src/br/com/senai/view/Menu.java
+++ src/br/com/senai/view/Menu.java
@@ -7,22 +7,22 @@ public class Menu {
 
 	public void menu() {
 		
-		// utilizando StringBuilder por pura estética.
+		// utilizando StringBuilder por pura estÃ©tica.
 		StringBuilder builder = new StringBuilder();
 		Scanner read = new Scanner(System.in);
 		
 		builder.append("(1) Gerenciamento de Produtos \n");
 		builder.append("(2) Gerenciamento de Compra e Venda \n");
 		builder.append("(3) Sair \n");
-		builder.append("Escolha uma opção: ");
+		builder.append("Escolha uma opÃ§Ã£o: ");
 		System.out.print(builder.toString());
 		
 		
-		// Switch utilizado para as opções do menu
+		// Switch utilizado para as opÃ§Ãµes do menu
 		switch(read.nextInt()) {
 		case 1:
 			GerenciamentoDeProduto ger = new GerenciamentoDeProduto();
-			ger.menu();
+			ger.menuGerenciamento();
 			break;
 		case 2:
 			
